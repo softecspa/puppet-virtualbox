ff672986ed2c39b093a9305b2b058594673ece1a
Fx <fpizzurro@nb-pizzurro.(none)>
2014-05-26 12:08:53 +0200

    aggiunto supporto per ubuntu

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class virtualbox(
-  $version      = "4.3.12",
-  $tmp_dir      = "C:\\vagrant-init\\tmp"
+  $version      = '4.3.12',
+  $tmp_dir      = '',
+  $use_apt      = true
 ) {
 
   require cygwin
@@ -15,30 +16,56 @@ class virtualbox(
       $filename         = "${tmp_dir}\\VirtualBox-${version}-93733-Win.exe"
       $provider         = "windows"
       $install_options  = [ "--silent" ]
+      $pkg_name         = "Oracle VM VirtualBox ${version}"
+      $ensure           = 'installed'
 
 
       cygwin::wget {$source:
         path    => $filename,
-        before  => Package["Oracle VM VirtualBox ${version}"],
+        before  => Package[$pkg_name],
+      }
+
+      windows_path{'virtualbox':
+        ensure    => 'present',
+        directory => 'C:\Program Files\Oracle\VirtualBox',
+        require   => Package[$pkg_name]
+      }
+    }
+
+    'ubuntu': {
+      $source           = $use_apt?{
+        true  => undef,
+        false => "http://download.virtualbox.org/virtualbox/${version}/virtualbox-4.3_${version}-93733~Ubuntu~${::lsbdistcodename}_${::architecture}.deb"
+      }
+      $filename         = "${tmp_dir}/virtualbox-4.3_${version}-93733~Ubuntu~${::lsbdistcodename}_${::architecture}.deb"
+      $provider         = $use_apt?{
+        true  => 'apt',
+        false => 'dpkg'
+      }
+      $install_options  = undef
+      $pkg_name         = 'virtualbox'
+      $ensure           = $version
+
+      if !$use_apt {
+        exec {'download virtualbox deb':
+          command => "wget $source -O $filename",
+          creates => $filename,
+          path    => $::path,
+          before  => Package[$pkg_name]
+        }
       }
     }
   }
 
-  package {"Oracle VM VirtualBox ${version}":
-    ensure          => installed,
+  package {$pkg_name:
+    ensure          => $ensure,
     source          => $filename,
     provider        => $provider,
     install_options => $install_options,
     notify          => Exec['install extpack'],
   }
 
-  windows_path{'virtualbox':
-    ensure    => 'present',
-    directory => 'C:\Program Files\Oracle\VirtualBox',
-    require   => Package["Oracle VM VirtualBox ${version}"]
-  }
-
-  class {'virtualbox::extension_pack':
-    tmp_dir => $tmp_dir
-  }
+  #class {'virtualbox::extension_pack':
+  #  tmp_dir => $tmp_dir
+  #}
 }
