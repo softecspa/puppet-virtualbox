a8d9da4efd6ff96846a2ab88875d9987c1306c87
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-05-30 14:57:29 +0000

    installazione per ubuntu da .deb

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,14 @@
 class virtualbox(
-  $version      = '4.3.12',
+  $version      = '4.3.12-93733',
   $tmp_dir      = '',
 ) {
 
-  if $version !~ /^4\.3\./ {
-    fail("only major version 4.3 is supported")
+  if $version !~ /^4\.3\.12/ {
+    fail("only major version 4.3.12 is supported")
   }
+
+  $minor_part = inline_template("<%= @version.split('-').at(-1) %>")
+  $major_version = regsubst($version,$minor_part,'')
 
   case $::operatingsystem {
     "windows":  {
@@ -15,11 +18,11 @@ class virtualbox(
         fail('in windows env you have to specify a tmp_dir')
       }
 
-      $source           = "http://download.virtualbox.org/virtualbox/${version}/VirtualBox-${version}-93733-Win.exe"
-      $filename         = "${tmp_dir}\\VirtualBox-${version}-93733-Win.exe"
+      $source           = "http://download.virtualbox.org/virtualbox/${major_version}/VirtualBox-${version}-Win.exe"
+      $filename         = "${tmp_dir}\\VirtualBox-${version}-Win.exe"
       $provider         = "windows"
       $install_options  = [ "--silent" ]
-      $pkg_name         = "Oracle VM VirtualBox ${version}"
+      $pkg_name         = "Oracle VM VirtualBox ${major_version}"
       $ensure           = 'installed'
       $notify           = Exec['install extpack']
 
@@ -34,24 +37,29 @@ class virtualbox(
         directory => 'C:\\Program Files\\Oracle\\VirtualBox',
         require   => Package[$pkg_name]
       }
+
+      package {$pkg_name:
+        ensure          => $ensure,
+        source          => $filename,
+        provider        => $provider,
+        install_options => $install_options,
+        notify          => $notify
+      }
     }
 
     'ubuntu': {
-      $pkg_name         = 'virtualbox'
-      $ensure           = latest
+      $pkg_name         = 'virtualbox-4.3'
+      $ensure           = present
       $source           = undef
       $provider         = 'apt'
       $install_options  = undef
       $notify           = undef
-    }
-  }
 
-  package {$pkg_name:
-    ensure          => $ensure,
-    source          => $filename,
-    provider        => $provider,
-    install_options => $install_options,
-    notify          => $notify
+      deb::from_url {$pkg_name :
+        url     => "http://download.virtualbox.org/virtualbox/${major_version}/${pkg_name}_${version}~Ubuntu~${::lsbdistcodename}_${::architecture}.deb",
+        version => $version
+      }
+    }
   }
 
   class {'virtualbox::extension_pack':
